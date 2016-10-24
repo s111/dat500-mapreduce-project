8e58bcdb8260f5e5586b65a00a2fc14f55bb5711
phaland <phaland@users.noreply.github.com>
2016-10-24 14:48:07 +0200

    Renamed class

--- mr/count_days.py
+++ mr/count_days.py
@@ -3,9 +3,7 @@ from mrjob.job import MRJob
 class MRCountDays(MRJob):
 
     def mapper(self, _, line):
-        #line = line.strip() # remove leading and trailing whitespace
         if line.startswith("Date: "):
-            #idx = 6  # line.startswith("Date: ")+1
             day = line[6:9]
             if day.lower() in ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']:
                 yield day.lower(), 1
