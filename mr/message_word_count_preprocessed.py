462cdc41c22c939cbaa596d527415fe9b39fb26d
Sebastian M. Pedersen <sebastian@code.64b.io>
2016-10-29 16:42:20 +0200

    Made script for preprocessing emails.csv
    Created an alternate message-word-count script that makes use of the new
    dataset.

---
+++ mr/message_word_count_preprocessed.py
@@ -0,0 +1,38 @@
+import re
+from collections import Counter
+
+from mrjob.job import MRJob
+from mrjob.protocol import JSONProtocol
+from nltk.corpus import words
+
+MESSAGE_ID = "\",\"Message-ID: "
+WORD = re.compile("\w+")
+
+
+class MRMessageWordCount(MRJob):
+    INPUT_PROTOCOL = JSONProtocol
+
+    def mapper_init(self):
+        self.vocabulary = {word.lower(): 0 for word in set(words.words())}
+        self.words = Counter()
+
+    def mapper(self, id, email):
+        _, message = email
+        words = (term for term in WORD.findall(message)
+                 if term in self.vocabulary)
+
+        self.words.update(words)
+
+    def mapper_final(self):
+        for word, occurences in self.words.items():
+            yield word, occurences
+
+    def reducer(self, word, occurences):
+        count = sum(occurences)
+
+        if count >= 10:
+            yield word, count
+
+
+if __name__ == "__main__":
+    MRMessageWordCount.run()
---
+++ mr/preprocess.py
@@ -0,0 +1,61 @@
+"""
+MRPreprocess preprocesses emails.csv into the format (id, (headers, message)).
+This is done for both performance and clarity of code. The headers are
+separated by the constant SEPERATOR so that they might be reconstucted at a
+later time, i.e. for being used by a library that expects the headers to be in
+their original format. In the body of the email new lines are replaced by
+spaces, as we o not care about the structure.
+
+If the output of this job is to be correct, we must ensure that the input data
+is not split. i.e one mapper receives the whole dataset. This can be done by
+setting the mapreduce.input.fileinputformat.split.minsize to the size of the
+dataset.
+"""
+
+from mrjob.job import MRJob
+
+MESSAGE_ID = "\",\"Message-ID: "
+SEPERATOR = "\30"  # ASCII record separator
+
+
+class MRPreprocess(MRJob):
+    def mapper_init(self):
+        self.buffer_lines = False
+        self.id = ""
+        self.headers = []
+        self.lines = []
+
+    def mapper(self, _, line):
+        if MESSAGE_ID in line:
+            if self.id:
+                headers = SEPERATOR.join(self.headers).lower()
+                message = " ".join(self.lines)
+
+                yield self.id, (headers, message)
+
+                self.headers = []
+                self.lines = []
+
+            self.id = line[line.find(MESSAGE_ID) + len(MESSAGE_ID) + 1:-1]
+            self.buffer_lines = False
+        elif not line and not self.buffer_lines:
+            self.buffer_lines = True
+        elif self.buffer_lines:
+            self.lines.append(line)
+        elif self.id:
+            if SEPERATOR in line:
+                raise Exception("SEPERATOR: {} found in header!".format(
+                    repr(SEPERATOR)))
+
+            self.headers.append(line)
+
+    def mapper_final(self):
+        if self.id:
+            headers = " ".join(self.headers).lower()
+            message = " ".join(self.lines).lower()
+
+            yield self.id, (headers, message)
+
+
+if __name__ == "__main__":
+    MRPreprocess.run()
