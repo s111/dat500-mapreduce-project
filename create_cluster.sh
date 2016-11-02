075530e984f39c6dbca2bfb9be88a54f58904801
Sebastian M. Pedersen <sebastian@code.64b.io>
2016-11-02 14:10:42 +0100

    Fixed bug in access keys loading

--- create_cluster.sh
+++ create_cluster.sh
@@ -3,6 +3,6 @@
 source ~/rootkey.csv
 
 export AWS_ACCESS_KEY_ID=${AWSAccessKeyId/%$'\r'/}
-export AWS_SECRET_ACCESS_KEY=${AWSSecretAccessKey/%$'\r'/}
+export AWS_SECRET_ACCESS_KEY=${AWSSecretKey/%$'\r'/}
 
 export CLUSTER_ID=`mrjob create-cluster -c mrjob_emr.conf`
--- load_access_keys.sh
+++ load_access_keys.sh
@@ -3,4 +3,4 @@
 source ~/rootkey.csv
 
 export AWS_ACCESS_KEY_ID=${AWSAccessKeyId/%$'\r'/}
-export AWS_SECRET_ACCESS_KEY=${AWSSecretAccessKey/%$'\r'/}
+export AWS_SECRET_ACCESS_KEY=${AWSSecretKey/%$'\r'/}
