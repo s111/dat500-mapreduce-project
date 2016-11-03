f757543f39cc21ce277e8f379bf27d9c2071a5e9
Sebastian M. Pedersen <sebastian@code.64b.io>
2016-11-03 15:24:31 +0100

    Removed excess .py extension

--- mapreduce/wordoccurences/tests.sh
+++ mapreduce/wordoccurences/tests.sh
@@ -2,7 +2,7 @@
 
 # We don't care about time. This is only see the data loss.
 python3 yield_early.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only > count.txt
-python3 in_mapper_combiner.py.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
+python3 in_mapper_combiner.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
 python3 in_mapper_combiner_valid_only.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
 python3 with_preprocessed_data.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --total-only >> count.txt
 python3 with_custom_format.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
@@ -10,7 +10,7 @@ python3 with_custom_format.py --python-archive MRCount.py -r hadoop hdfs:///user
 # perf is only used for running the command n times, and also for seeing the total time used.
 # We inspect the logs for the actual time.
 perf stat -r 3 -d python3 yield_early.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
-perf stat -r 3 -d python3 in_mapper_combiner.py.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 in_mapper_combiner.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
 perf stat -r 3 -d python3 in_mapper_combiner_valid_only.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
 perf stat -r 3 -d python3 with_preprocessed_data.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --no-output
 perf stat -r 3 -d python3 with_custom_format.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
