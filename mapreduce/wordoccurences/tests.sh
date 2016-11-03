0d9ac8a704c54a4d488edf9a6e1a6e881c3abcc0
Sebastian M. Pedersen <sebastian@code.64b.io>
2016-11-03 21:40:12 +0100

    Set buffer.size to 128K in tests too

--- mapreduce/wordoccurences/tests.sh
+++ mapreduce/wordoccurences/tests.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # We don't care about time. This is only see the data loss.
-python3 yield_early.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only > count.txt
-python3 in_mapper_combiner.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
-python3 in_mapper_combiner_valid_only.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
-python3 with_preprocessed_data.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --total-only >> count.txt
-python3 with_custom_format.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
+python3 yield_early.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only > count.txt
+python3 in_mapper_combiner.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
+python3 in_mapper_combiner_valid_only.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
+python3 with_preprocessed_data.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --total-only >> count.txt
+python3 with_custom_format.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --total-only >> count.txt
 
 # perf is only used for running the command n times, and also for seeing the total time used.
 # We inspect the logs for the actual time.
-perf stat -r 3 -d python3 yield_early.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
-perf stat -r 3 -d python3 in_mapper_combiner.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
-perf stat -r 3 -d python3 in_mapper_combiner_valid_only.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
-perf stat -r 3 -d python3 with_preprocessed_data.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --no-output
-perf stat -r 3 -d python3 with_custom_format.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 yield_early.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 in_mapper_combiner.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 in_mapper_combiner_valid_only.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 with_preprocessed_data.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --no-output
+perf stat -r 3 -d python3 with_custom_format.py --jobconf "io.file.buffer.size=131072" --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
