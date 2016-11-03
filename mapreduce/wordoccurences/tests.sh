a46e2e5684b52417c5bcac9eada91b26b64e369f
Sebastian M. Pedersen <sebastian@code.64b.io>
2016-11-03 15:16:20 +0100

    Fixed misplaced argument

--- mapreduce/wordoccurences/tests.sh
+++ mapreduce/wordoccurences/tests.sh
@@ -9,8 +9,8 @@ python3 with_custom_format.py --python-archive MRCount.py -r hadoop hdfs:///user
 
 # perf is only used for running the command n times, and also for seeing the total time used.
 # We inspect the logs for the actual time.
-perf stat --python-archive MRCount.py -r 3 -d python3 yield_early.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
-perf stat --python-archive MRCount.py -r 3 -d python3 in_mapper_combiner.py.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
-perf stat --python-archive MRCount.py -r 3 -d python3 in_mapper_combiner_valid_only.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
-perf stat --python-archive MRCount.py -r 3 -d python3 with_preprocessed_data.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --no-output
-perf stat --python-archive MRCount.py -r 3 -d python3 with_custom_format.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 yield_early.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 in_mapper_combiner.py.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 in_mapper_combiner_valid_only.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
+perf stat -r 3 -d python3 with_preprocessed_data.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails_preprocessed.csv --no-output
+perf stat -r 3 -d python3 with_custom_format.py --python-archive MRCount.py -r hadoop hdfs:///user/hadoop/emails.csv --no-output
